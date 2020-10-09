6b48ed5102b18113e3f2139951ee1a0485241276
luiz filipe de moura <luizfilipeholiday@gmail.com>
2020-10-09 06:12:39 -0400

    eh isto

---
+++ public/README.md
@@ -0,0 +1,3 @@
+# Agenda
+
+Repositório dedicado ao trabalho da disciplina de Programação para Web que visa desenvolver uma aplicação que auxilia a organização de sua agenda  
---
+++ script_meses.js
@@ -0,0 +1,34 @@
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBMasMxL8BhY9OSyNkAmMZqfMtSlaYDEp8",
+    authDomain: "weebdario.firebaseapp.com",
+    databaseURL: "https://weebdario.firebaseio.com",
+    projectId: "weebdario",
+    storageBucket: "weebdario.appspot.com",
+    messagingSenderId: "742261135078",
+    appId: "1:742261135078:web:67e5be9399087e69efcd27"
+};
+
+
+var user = {};
+
+var mes = new Date().getMonth();
+var diaDaSemanaQueOMesComeca = new Date(2020, mes,1).getDay();
+var diasNosMeses = [31,28,31,30,31, 30,31,31,30,31,30,31];
+var meses = ['Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'];
+function getUser() {
+    return JSON.parse(localStorage.getItem('user'));
+}
+
